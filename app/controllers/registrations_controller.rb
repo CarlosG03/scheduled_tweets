83f36ff6c9496863b407d0d59a42e12c3a65833c
Carlos Garro <cgarro@macbook-pro.fios-router.home>
2021-05-27 09:06:36 -0400

    Adds sign in and sign up features to the navbar 	Accessing current users 	Logging out Form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,15 @@ class RegistrationsController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id]= @user.id
             redirect_to root_path, notice: "Successful Sign Up"
         else
             render :new
         end
     end
+    
+    private
+    
     def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
     end
