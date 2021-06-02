c3a4ff999c2737d8630797c11da31fafc45124aa
Carlos Garro <cgarro@bdtrust.org>
2021-06-02 18:50:15 -0400

    Model and view fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,11 +2,17 @@ class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
 
-  validates :body, length: {minimum: 1, maximum:280 }
+  validates :body, length: { minimum: 1, maximum: 280 }
   validates :publish_at, presence: true
 
   after_initialize do
-    self.publish_at ||= 1.hour.from_now
+    self.publish_at ||= 24.hours.from_now
+  end
+
+  after_save_commit do
+    if publish_at_previously_changed?
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
   end
 
   def published?
@@ -17,6 +23,4 @@ class Tweet < ApplicationRecord
     tweet = twitter_account.client.update(body)
     update(tweet_id: tweet.id)
   end
-
-
 end
